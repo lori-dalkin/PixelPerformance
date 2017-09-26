8e4408e1554aa1dfd2a5280c0f95ec7e2a16d4c1
Jacob Gagne <jacob_gagne@hotmail.com>
2017-09-26 00:34:02 -0400

    first steps to completing login frontend

--- client/dev/client/components/App.js
+++ client/dev/client/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
+import Toolbar from 'material-ui/Toolbar'; 
 import Typography from 'material-ui/Typography';
-import { withStyles } from 'material-ui/styles';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { withStyles, MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import blue from 'material-ui/colors/blue';
+import pink from 'material-ui/colors/pink';
 require('../../scss/style.scss');
+
+import Login from './authentication/Login';
 
 const styles = theme => ({
 	root: {
@@ -12,19 +17,33 @@ const styles = theme => ({
   }
 });
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blue, // Purple and green play nicely together.
+    secondary: pink
+  }
+});
+
 function App(props) {
 	
 	const classes = props.classes;
 	return (
-	  <div className={classes.root}>
-	    <AppBar position="static">
-		    <Toolbar>
-		    	<Typography type="title" color="inherit">
-		        Soen 343
-		      </Typography>
-	      </Toolbar>
-	    </AppBar>
-	  </div>)
+		<Router>
+		  <MuiThemeProvider theme={theme}>
+			  <div className={classes.root}>
+			    <AppBar position="static">
+				    <Toolbar>
+				    	<Typography type="title" color="inherit">
+				        Fin Fin Tech Application
+				      </Typography>
+			      </Toolbar>
+			    </AppBar>
+			    <Route exact path="/" />
+			    <Route path="/admin" component={Login} />
+			  </div>
+		  </MuiThemeProvider>
+	  </Router>
+	  )
 
 };
 
