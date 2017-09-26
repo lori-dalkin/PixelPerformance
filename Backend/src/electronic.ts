a7572164585db21e65928af0725dd3a8fdfda4f4
Bryce <BDSchoeler@gmail.com>
2017-09-26 16:39:14 -0400

    Added electronic class and db file.

---
+++ Backend/src/electronic.ts
@@ -0,0 +1,15 @@
+abstract class Electronic {
+    id: number;
+	weight: number ;
+	modelNumber: string;
+	brand: string;
+	price: number;
+    constructor(id: number, weight: number, modelNumber: string, brand: string, price: number) {
+		this.id = id;
+		this.weight = weight;
+		this.modelNumber = modelNumber;
+		this.brand = brand;
+		this.price = price;
+    }
+    abstract save():boolean;
+}
