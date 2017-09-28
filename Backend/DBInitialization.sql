8398838b908f835ef8b69c9f8d0c99b280051e45
Claudia Della Serra <claudiadellaserra@gmail.com>
2017-09-27 20:39:23 -0400

    Added desktop class + updated db initialization script

--- Backend/DBInitialization.sql
+++ Backend/DBInitialization.sql
@@ -1,10 +1,25 @@
-CREATE TABLE monitors (
-    id int,
-	weight double,
-	modelNumber varchar(20),
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE monitors(
+    id UUID DEFAULT uuid_generate_v1(),
+	weight dec(4, 2),
+	modelNumber varchar(20) UNIQUE,
 	brand varchar(30),
 	price dec(6,2),
     size int,
+	PRIMARY KEY(id)
 );
-ALTER TABLE monitors 
-   ADD CONSTRAINT PK_monitors PRIMARY KEY ID;
+
+CREATE TABLE desktops(
+	id UUID DEFAULT uuid_generate_v1(),
+	weight dec(4, 2),
+	modelNumber varchar(20) UNIQUE,
+	brand varchar(30),
+	price dec(6, 2),
+	processor varchar(20),
+	ram int,
+	cpus int,
+	hardDrive int,
+	dimensions real,
+	PRIMARY KEY(id)
+);
