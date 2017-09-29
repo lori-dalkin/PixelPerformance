bc5004bb6d78c7a5408ba2ad47b0bb7f2624825d
Claudia Della Serra <claudiadellaserra@gmail.com>
2017-09-28 23:28:54 -0400

    Fixed db initalize error + added load functions

--- Backend/src/catalog.ts
+++ Backend/src/catalog.ts
@@ -2,6 +2,8 @@ import { Electronic } from "./electronic"
 import { Monitor } from "./monitor"
 import { dbconnection } from "./dbconnection"
 import {Desktop} from "./desktop";
+import {Tablet} from "./tablet";
+import {TelevisionSet} from "./TelevisionSet";
 
 var db = new dbconnection().getDBConnector();
 export class Catalog {
@@ -12,6 +14,8 @@ export class Catalog {
 		this.electronics = [];
 		this.loadMonitors();
 		this.loadDesktops();
+		this.loadTablets();
+		this.loadTelevisions();
 	}
 
 	private loadMonitors(): void {
@@ -20,7 +24,7 @@ export class Catalog {
         db.many('SELECT * FROM monitors')
             .then(function(rows){
             	for(let row of rows){
-                	monitor = new Monitor(row.id,row.weight,row.modelNumber, row.brand, row.price, row.size);
+                	monitor = new Monitor(row.id, row.weight, row.modelNumber, row.brand, row.price, row.size);
                 	monitors.push(monitor);
 				}
             }).catch(function (err) {
@@ -34,7 +38,7 @@ export class Catalog {
         db.many('SELECT * FROM desktops')
             .then(function(rows){
                 for(let row of rows){
-                    desktop = new Desktop(row.id,row.weight,row.modelNumber, row.brand, row.price, row.processor, row.ram, row.cpus, row.hardDrive, row.os, row.dimensions)
+                    desktop = new Desktop(row.id, row.weight, row.modelNumber, row.brand, row.price, row.processor, row.ram, row.cpus, row.hardDrive, row.os, row.dimensions)
 					desktops.push(desktop);
 					console.log(desktops);
                 }
@@ -43,10 +47,41 @@ export class Catalog {
 		});
 	}
 
+    private loadTablets(): void {
+        let tablet:Tablet;
+        let tablets = this.electronics;
+        db.many('SELECT * FROM tablets')
+            .then(function(rows){
+                for(let row of rows){
+                    tablet = new Tablet(row.id, row.weight, row.modelNumber, row.brand, row.price, row.processor, row.ram, row.cpus, row.hardDrive, row.os, row.displaySize, row.dimensions, row.battery, row.camera)
+                    tablets.push(tablet);
+                    console.log(tablet);
+                }
+            }).catch(function (err) {
+            console.log("No tablets found"+ err);
+        });
+    }
+
+    private loadTelevisions(): void {
+        let tv:TelevisionSet;
+        let tvs = this.electronics;
+        db.many('SELECT * FROM tablets')
+            .then(function(rows){
+                for(let row of rows){
+                    tv = new TelevisionSet(row.id,row.weight,row.modelNumber, row.brand, row.price, row.dimensions, row.type);
+                    tvs.push(tv);
+                    console.log(tv);
+                }
+            }).catch(function (err) {
+            console.log("No tablets found"+ err);
+        });
+    }
+
 	public getProduct(productId:string): Electronic {
+		let elecIterator = this.electronics;
 		for(var iter = 0; iter < this.electronics.length; iter++){
-			if(productId == this.electronics[iter].getId())
-				return this.electronics[iter];
+			if(productId == elecIterator[iter].getId())
+				return elecIterator[iter];
 		}
 		return null;
 	}
