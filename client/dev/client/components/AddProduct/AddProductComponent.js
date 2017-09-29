1e2d55a03391bc025cbebfabf10f683eec4d5763
Samantha Kerr <samantha-kerr@hotmail.ca>
2017-09-29 09:33:27 -0400

    #20 Refactoring form

--- client/dev/client/components/AddProduct/AddProductComponent.js
+++ client/dev/client/components/AddProduct/AddProductComponent.js
@@ -1,15 +1,8 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
 
 import TextField from 'material-ui/TextField';
-import Button from 'material-ui/Button'
 import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
-
-import MonitorFormElements from '/MonitorFormElements';
-import ComputerSystemFormElements from '/ComputerSystemFormElements';
-import TelevisionSetFormElements from '/TelevisionSetFormElements';
+import Button from 'material-ui/Button';
 
 export class AddProductComponent extends Component {
 
@@ -38,6 +31,8 @@ export class AddProductComponent extends Component {
         };
         this.updateState = this.updateState.bind(this);
         this.addProduct = this.addProduct.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
     }
 
     updateState(event) {
@@ -51,24 +46,32 @@ export class AddProductComponent extends Component {
         // Add product function.
     }
 
+    handleClose(event){
+        // Add close function.
+    }
+
+    handleOpen(event){
+        // Add open function.
+    }
+
     render(){
         const actions =[
-            <FlatButton
+            <Button
                 label="Cancel"
                 primary={true}
                 onClick={this.handleClose}
             />,
-            <FlatButton
+            <Button
                 label="Submit"
                 primary={true}
                 disabled={true}
                 onClick={this.handleClose}
-            />,
+            />
         ];
 
         return (
             <div>
-                <RaisedButton label="Add Product Model" onClick={this.handleOpen} />
+                <Button label="Add Product Model" onClick={this.handleOpen} />
                 <Dialog
                     title="Add Product Form"
                     actions={actions}
@@ -117,19 +120,173 @@ export class AddProductComponent extends Component {
                             <MenuItem value="monitor" primaryText="Monitor"/>
                         </SelectField>
                         <div>
-                            if (this.state.category.equals('televisionSet')){
-                                <TelevisionSetFormElements dimensions={this.state.dimensions} type={this.state.type} />
-                            }else if (props.computerType.equals('computerSystem')){
-                                <ComputerSystemFormElements props={this.state} />
-                            }else if (props.computerType.equals('monitor')){
-                                <MonitorFormElements props={this.state} />
-                            }else{
-                                <p />
-                            }
+                            {(() => {
+                                switch (this.state.category) {
+                                    case "televisionSet":
+                                        return (
+                                            <div>
+                                                <TextField
+                                                    id="dimensions"
+                                                    name="dimensions"
+                                                    label="Dimensions"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <TextField
+                                                    id="type"
+                                                    name="type"
+                                                    label="Type"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                            </div>
+                                        );
+                                    case "computerSystem":
+                                        return (
+                                            <div>
+                                                <TextField
+                                                    id="processor"
+                                                    name="processor"
+                                                    label="Processor"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <TextField
+                                                    id="ram"
+                                                    name="ram"
+                                                    label="RAM"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <TextField
+                                                    id="hardDrive"
+                                                    name="hardDrive"
+                                                    label="Harddrive"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <TextField
+                                                    id="cpu"
+                                                    name="cpu"
+                                                    label="CPU"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <TextField
+                                                    id="os"
+                                                    name="os"
+                                                    label="OS"
+                                                    value={this.state.value}
+                                                    onChange={this.updateState}
+                                                />
+                                                <SelectField name="computerType" value={this.state.value} onChange={this.updateState}>
+                                                    <MenuItem value="desktop" primaryText="Desktop"/>
+                                                    <MenuItem value="laptop" primaryText="Laptop"/>
+                                                    <MenuItem value="tablet" primaryText="Tablet"/>
+                                                </SelectField>
+                                                <div>
+                                                    {(() => {
+                                                        switch (this.state.computerType) {
+                                                            case "desktop":
+                                                                return (
+                                                                    <div>
+                                                                        <TextField
+                                                                            id="dimensions"
+                                                                            name="dimensions"
+                                                                            label="Dimensions"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                    </div>
+                                                                );
+                                                            case "laptop":
+                                                                return (
+                                                                    <div>
+                                                                        <TextField
+                                                                            id="displaySize"
+                                                                            name="displaySize"
+                                                                            label="Display Size"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="battery"
+                                                                            name="battery"
+                                                                            label="Battery"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="camera"
+                                                                            name="camera"
+                                                                            label="Camera"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="touchScreen"
+                                                                            name="touchScreen"
+                                                                            label="Touch Screen"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                    </div>
+                                                                );
+                                                            case "tablet":
+                                                                return (
+                                                                    <div>
+                                                                        <TextField
+                                                                            id="displaySize"
+                                                                            name="displaySize"
+                                                                            label="Display Size"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="battery"
+                                                                            name="battery"
+                                                                            label="Battery"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="camera"
+                                                                            name="camera"
+                                                                            label="Camera"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                        <TextField
+                                                                            id="dimension"
+                                                                            name="dimensions"
+                                                                            label="Dimensions"
+                                                                            value={this.state.value}
+                                                                            onChange={this.updateState}
+                                                                        />
+                                                                    </div>
+                                                                );
+                                                            default:
+                                                                return <p />;
+                                                        }
+                                                    })()}
+                                                </div>
+                                            </div>
+                                        );
+                                    case "monitor":
+                                        return (
+                                            <TextField
+                                                id="size"
+                                                name="size"
+                                                label="Size"
+                                                value={this.state.value}
+                                                onChange={this.updateState}
+                                            />
+                                        );
+                                    default:
+                                        return <p />;
+                                }
+                            })()}
                         </div>
-
-                        <Button type="submit">Submit</Button>
-                        <Button type="button" onClick={reset}>Clear</Button>
                     </form>
                 </Dialog>
             </div>
