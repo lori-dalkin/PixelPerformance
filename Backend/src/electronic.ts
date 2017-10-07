2d148f83645bea5eb3916c4dc4565ac65461d204
Claudia Della Serra <claudiadellaserra@gmail.com>
2017-10-07 13:06:56 -0400

    Updated function visibility to allow compilation

--- Backend/src/electronic.ts
+++ Backend/src/electronic.ts
@@ -16,24 +16,24 @@ export abstract class Electronic {
 		this.electronicType = electronicType;
     }
 
-    protected getId(): string{ return this.id;}
-    protected getWeight(): number { return this.weight;}
-    protected getModelNumber(): string { return this.modelNumber;}
-    protected getBrand(): string { return this.brand;}
-    protected getPrice(): number { return this.price;}
-    protected getElectronicType(): string { return this.electronicType; }
+    public getId(): string{ return this.id;}
+    public getWeight(): number { return this.weight;}
+    public getModelNumber(): string { return this.modelNumber;}
+    public getBrand(): string { return this.brand;}
+    public getPrice(): number { return this.price;}
+    public getElectronicType(): string { return this.electronicType; }
 
-	protected setId(id: string): void { this.id = id; }
-	protected setWeight(weight: number): void { this.weight = weight; }
-	protected setModelNumber(modelNum: string): void { this.modelNumber = modelNum; }
-	protected setBrand(brand: string): void { this.brand = brand; }
-	protected setPrice(price: number): void { this.price = price; }
-	protected setElectronicType(elecType: string): void { this.electronicType = elecType; }
+    public setId(id: string): void { this.id = id; }
+    public setWeight(weight: number): void { this.weight = weight; }
+    public setModelNumber(modelNum: string): void { this.modelNumber = modelNum; }
+    public setBrand(brand: string): void { this.brand = brand; }
+    public setPrice(price: number): void { this.price = price; }
+    public setElectronicType(elecType: string): void { this.electronicType = elecType; }
 
     abstract save():boolean;
 
-    abstract modify(): boolean;
-
-    abstract delete(): boolean;
+    // abstract modify(): boolean;
+    //
+    // abstract delete(): boolean;
 
 }
