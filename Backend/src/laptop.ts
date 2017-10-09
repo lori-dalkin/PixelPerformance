990561d29733cf22d834b5df25bb7649d8c006be
Dylan Fernandes <dylangfernandes@gmail.com>
2017-10-09 15:15:25 -0400

    updated laptop class with new methods

--- Backend/src/laptop.ts
+++ Backend/src/laptop.ts
@@ -5,10 +5,10 @@ import { Electronic } from "./electronic";
 var db = new dbconnection().getDBConnector();
 
 export class Laptop extends ComputerSystem {
-	displaySize: number;
-	battery: number;
-	camera: boolean;
-	touchscreen: boolean;
+	private displaySize: number;
+	private battery: number;
+	private camera: boolean;
+	private touchscreen: boolean;
 
 	constructor(id: string, weight: number, modelNumber: string, brand: string,
 				price: number, processor: string, ram: number, cpus: number,
@@ -21,36 +21,23 @@ export class Laptop extends ComputerSystem {
 		this.camera = camera;
 		this.touchscreen = touchscreen;
 	}
+ 	/****************************************************************
+     * Getters and Setters											*
+     ****************************************************************/
+	public getDisplaySize():number {return this.displaySize;}
+	public getBattery():number {return this.battery;}
+	public getCamera():boolean {return this.camera;}
+	public getTouchscreen():boolean {return this.touchscreen;}
+
+	public setDisplaySize(displaySize:number) {this.displaySize = displaySize;}
+	public setBattery(battery:number) {this.battery = battery;}
+	public setCamera(isCamera:boolean ){this.camera = isCamera;}
+	public setTouchscreen(isTouchscreen:boolean) {this.touchscreen=isTouchscreen;}
 
 	/**************************************************************
 	 * Method to persist an object of type Laptop to the database *
 	 **************************************************************/
 	save(): boolean {
-		let queryParameters = ["'"+this.id+"'", this.weight,"'"+ this.modelNumber +"'",
-								"'"+this.brand +"'", this.price, "'"+this.processor +"'",
-							   this.ram, this.cpus, this.hardDrive,
-							   "'"+this.os+"'", this.displaySize, this.battery,
-							   this.camera, this.touchscreen];
-		let queryText = 'INSERT INTO laptops SET weight=' + this.weight + ',modelNumber=' + this.modelNumber + ',brand=' + this.brand
-						+ ',price=' + this.price + ', processor=' + this.processor + ',ram' + this.ram + ',cpus=' + this.cpus 
-						+ ',hardDrive=' + this.hardDrive + ',os=' + this.os + ',displaySize=' + this.displaySize + ',battery='+ this.battery
-						+ ',camera=' + this.camera + ',touchscreen=' +this.touchscreen + 'WHERE id =' + this.id;
-
-		db.none(queryText)
-			.then(function() {
-				console.log("Laptop added to db");
-			})
-			.catch(function(err) {
-				console.log("Error updating Laptop in the db");
-				return false;
-			});
-		return true;
-	}
-
-	/**************************************************************
-	 * Method to persist an object of type Laptop to the database *
-	 **************************************************************/
-	update(): boolean {
 		let queryParameters = ["'"+this.id+"'", this.weight,"'"+ this.modelNumber +"'",
 								"'"+this.brand +"'", this.price, "'"+this.processor +"'",
 							   this.ram, this.cpus, this.hardDrive,
@@ -84,4 +71,51 @@ export class Laptop extends ComputerSystem {
 			})
 		return laptop;
 	}
+	/**************************************************************
+	 * Method to modify an object of type Laptop to the database *
+	 **************************************************************/
+	public async modify(): Promise<boolean> {
+		let queryText = 'INSERT INTO laptops SET weight=' + this.weight + ',modelNumber=' + this.modelNumber + ',brand=' + this.brand
+						+ ',price=' + this.price + ', processor=' + this.processor + ',ram' + this.ram + ',cpus=' + this.cpus 
+						+ ',hardDrive=' + this.hardDrive + ',os=' + this.os + ',displaySize=' + this.displaySize + ',battery='+ this.battery
+						+ ',camera=' + this.camera + ',touchscreen=' +this.touchscreen + 'WHERE id =' + this.id;
+
+		db.none(queryText)
+			.then(function() {
+				return true;
+			})
+			.catch(function(err) {
+				console.log("Error modifying Laptop in the db");
+				return false;
+			});
+		return true;
+	}
+	/**************************************************************
+	 * Method to delete an object of type Laptop to the database *
+	 **************************************************************/
+	public async delete(): Promise<boolean> {
+        db.none("DELETE FROM laptops WHERE id ='"+ this.getId() + "';")
+            .then(function () {
+                return true;
+            }).catch(function (err) {
+            console.log("No matching object found for delete: "+ err);
+            return false;
+        });
+        return true;
+	}
+	
+	/*******************************************************
+     * Method to return all laptops saved in the database
+     *******************************************************/
+    public static async findAll(){
+        let laptops: Electronic[];
+        let laptop: Laptop;
+        return db.many('SELECT * FROM laptops')
+            .then(function (rows) {
+                return rows as Electronic[];
+            }).catch(function (err) {
+                console.log("Error in getting all laptops:" + err);
+                return null;
+            });
+    }
 }
