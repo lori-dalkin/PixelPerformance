1625da656ac3fbab75919e4da1b7b4f6e984b299
Claudia Della Serra <claudiadellaserra@gmail.com>
2017-11-02 16:03:49 -0400

    Changed cart id and clientid to string

--- Backend/src/Models/cart.ts
+++ Backend/src/Models/cart.ts
@@ -1,20 +1,20 @@
 import { Inventory } from "./inventory";
 export class Cart {
     //This class will have an array of inventory objects, an id, and a userId.
-    private id:number;
-    private userId:number;
-    private inventory:Inventory[] = new Array<Inventory>();
+    private id: string;
+    private userId: string;
+    private inventory: Inventory[] = new Array<Inventory>();
 
-    constructor(newId:number, newUserId:number) {
+    constructor(newId: string, newUserId: string) {
         this.id = newId;
         this.userId = newUserId;
     }
-    public getId():number {return this.id;}
-    public setId(newId:number):void {this.id = newId;}
+    public getId(): string {return this.id;}
+    public setId(newId: string):void {this.id = newId;}
 
-    public getUserId():number {return this.userId;}
-    public setUserId(newUserId):void {this.userId = newUserId;}
+    public getUserId(): string {return this.userId;}
+    public setUserId(newUserId: string):void {this.userId = newUserId;}
 
-    public setInventory(inv:Inventory[]):void{ this.inventory  = inv;}
-    public getInventory():Inventory[]{ return this.inventory;}
+    public setInventory(inv: Inventory[]):void{ this.inventory  = inv;}
+    public getInventory(): Inventory[]{ return this.inventory;}
 }
