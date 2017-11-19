7581616124942bd6f54e0a6bcf7c186fc89c6082
Bryce <bdschoeler@gmail.com>
2017-11-19 00:19:52 -0500

    made get all client test better

--- Backend/src/test/usermanagement.spec.ts
+++ Backend/src/test/usermanagement.spec.ts
@@ -1,7 +1,11 @@
 import { UserManagement} from '../usermanagement';
 import { WebPortal } from '../webportal';
+import { dbconnection } from "../Models/dbconnection";
 import { expect } from 'chai';
+import { Client } from "../Models/client";
 import 'mocha';
+
+var db = new dbconnection().getDBConnector();
 let userManagement = UserManagement.getInstance();
 
 beforeEach(function (done) {
@@ -15,6 +19,15 @@ describe('Getting all clients', () => {
   it('should return a list of all clients', () => {
     
     const users = userManagement.getAllClients();
+    
+    db.many('SELECT * FROM clients')
+    .then(function (data) {
+        for(var i=0; i<data.length; i++){
+            let client = new Client(data[i].id,data[i].fname,data[i].lname,
+            data[i].email, data[i].password, data[i].address, data[i].phone);
+            expect(users[i]).to.deep.equal(client);
+        }
+    });
     for(let user of users){
       expect(user).to.have.property('id');
       expect(user).to.have.property('fname');
