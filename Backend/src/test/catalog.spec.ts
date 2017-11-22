280779edc0c61e7f1aacaff39e73d2498eb3270b
Daniel <ddicorpo@gmail.com>
2017-11-22 16:24:30 -0500

    WIP

--- Backend/src/test/catalog.spec.ts
+++ Backend/src/test/catalog.spec.ts
@@ -132,8 +132,10 @@ describe ('Adding an Inventory by ID', () => {
         let valueShouldBeTrue: boolean = true;
 
         //verify I can add the new monitor to my inventory;
-        var newMonitorAdded = catalog.addInventory(newTestMonitor.getId());
-        expect(newMonitorAdded).to.equal(valueShouldBeTrue);
+        let newMonitorAdded = catalog.addInventory(newTestMonitor.getId());
+        newMonitorAdded.then(function (newMonitorAdded) {
+            expect(newMonitorAdded).to.equal(valueShouldBeTrue);
+        })
 
     });
 });
