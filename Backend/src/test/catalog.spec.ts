3e182c8900d487953ccc5b043009196c6fec7453
Daniel <ddicorpo@gmail.com>
2017-11-22 17:56:46 -0500

    WIP

--- Backend/src/test/catalog.spec.ts
+++ Backend/src/test/catalog.spec.ts
@@ -220,3 +220,78 @@ describe('Deleting a Product by its ID', () => {
 
 });
 
+describe('Getting an Inventory by ID', () =>{
+    
+        it('return should be an electronic with certain ID', () => {
+    
+            //get array of inventories
+            const inventories = catalog.inventories
+    
+            // iterate throughout the array to verify the inventories field are all equal when using getInventory method
+            for (let inventory of inventories){
+                expect(inventory).to.equal(catalog.getInventory(inventory.getserialNumber()));
+                expect(inventory.getinventoryType()).to.equal(catalog.getInventory(inventory.getserialNumber()).getinventoryType());
+                expect(inventory.getLockedUntil()).to.equal(catalog.getInventory(inventory.getserialNumber()).getLockedUntil());
+                expect(inventory.getCartId()).to.equal(catalog.getInventory(inventory.getserialNumber()).getCartId());
+                expect(inventory.getReturnDate()).to.equal(catalog.getInventory(inventory.getserialNumber()).getReturnDate());
+            }
+        });
+    });
+    
+describe ('Adding an Inventory by ID', () => {
+
+    it('return true if Inventory is successfully added', () => {
+        
+        //add a product just verify that the a new item can be added to inventory too
+        let monitor = new Monitor(null, 1, "a", "b", 1, false, 1);
+        catalog.addProduct(monitor);
+
+        // get the array of electronics
+        const electronics = catalog.electronics;
+        
+        let newTestMonitor = electronics[electronics.length - 1];
+        let valueShouldBeTrue: boolean = true;
+
+        //verify I can add the new monitor to my inventory;
+        expect(catalog.addInventory(newTestMonitor.getId())).to.equal(valueShouldBeTrue);
+
+    });
+});
+
+describe('Deleting an Inventory by its ID', () => {
+    
+        it('true will be returned once the inventory is deleted', () => {
+    
+            //add a product just verify that the a new item can be added to inventory too
+            let monitor = new Monitor(null, 1, "model100", "b", 1, false, 1);
+            catalog.addProduct(monitor);
+
+            //get the array of electronics
+            const electronics = catalog.electronics;
+            
+            let newTestMonitor = electronics[electronics.length - 1];
+            let valueShouldBeTrue: boolean = true;
+
+            //adding the newly created monitor just to verify that it can be deleted too
+            var newMonitorAdded = catalog.addInventory(newTestMonitor.getId());
+
+            //verify I can delete the new monitor from my inventory
+            var newMonitorDeleted = catalog.deleteInventory(newTestMonitor.getId());
+            newMonitorDeleted.then(function (newMonitorDeleted){
+                expect(newMonitorDeleted).to.equal(valueShouldBeTrue);
+            })
+    
+            //try deleting all the other items within our electronic inventory
+            let length = catalog.electronics.length;
+            for (let i = 0; i <= length - 1; i++) {
+                var value = catalog.deleteInventory(catalog.electronics[0].getId());
+                value.then(function (value) {
+                    expect(value).to.equal(valueShouldBeTrue);
+                });
+            }
+    
+            //verify the array is null after deleting all the electronics, just to verify that the last loop effectively called deleteInventory(electronicID)
+            expect(0).to.equal(catalog.electronics.length);
+        });
+    
+    });
